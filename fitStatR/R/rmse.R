abdd301557a32d9271b0186ac62047ab55b2f2ce
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-20 16:38:54 -0500

    add rmse function for Root Mean Squared Error

---
+++ fitStatR/R/rmse.R
@@ -0,0 +1,15 @@
+#' Calculate Root Mean Squared Error (RMSE)
+#' 
+#' \code{rmse} calculates RMSE for statistical models.
+#' 
+#' @param y A numeric vector of length n (the number of observations) giving
+#'   the observed outcomes
+#' @param P A numeric matrix of n nows and m (the number of forecasting models)
+#'   columns, where each entry i,j is the predicted value of observation i
+#'   from forecasting model j
+#' 
+#' @return A numeric vector of length n whose values are the RMSE for each
+#'   forecasting model used to construct P
+rmse <- function(y, P){
+  return(apply(abs(P - y), 2, function(x) sqrt(sum(x^2)/length(y))))
+}
