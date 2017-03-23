569306931983b1e4b40d6ff6aa792b9e5d5461fb
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-23 07:54:12 -0500

    add check install and build lines to development file

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -6,3 +6,6 @@ setwd('~/Rclass/PS5') # change as appropriate
 current.code <- as.package('fitStatR')
 load_all(current.code)
 document(current.code)
+check(current.code)
+install(pkg=current.code, local=TRUE)
+build(current.code, path=getwd())
