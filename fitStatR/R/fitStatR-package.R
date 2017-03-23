b6e2af727cf485503d74f069bacdc511258fd0f7
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-23 08:49:34 -0500

    add package documentation, re-run document()

---
+++ fitStatR/R/fitStatR-package.R
@@ -0,0 +1,8 @@
+#' fitStatR
+#'
+#' The fitStatR package calculates fit statistics for forecasting models.  
+#' @name fitStatR
+#' @docType package
+#' @author  JB Duck-Mayr
+#' @importFrom stats median na.omit
+NULL
